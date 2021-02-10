6b4ec67cfae3292d4dc4a691aa2dbb4d24deee68
Sedriskor <comodexit@gmail.com>
2021-02-10 17:00:00 +0800

    ver.6 add check file exist

--- proudct_list.py
+++ proudct_list.py
@@ -1,12 +1,19 @@
 #讀取檔案
+import os # os = operating system
+
 proudcts = []
-with open('proudcts.csv', 'r', encoding='utf-8') as f:
-    for line in f:
-        if '商品,價格' in line:#過讀取'商品,價格'字串
-            continue#跳到下一迴
-        name, price = line.strip().split(',')
-        proudcts.append([name, price])
-print(proudcts)#檢查上面有沒有coding對
+if os.path.isfile('proudcts.csv'):#檢查檔案是否在現在資料夾內
+    print('有此檔案')
+    with open('proudcts.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:#跳過讀取'商品,價格'字串
+                continue#跳到下一迴
+            name, price = line.strip().split(',')
+            proudcts.append([name, price])
+    print(proudcts)#檢查上面有沒有跑對
+
+else:
+    print('沒有此檔案...')
 
 #讓使用者輸入
 while True:
@@ -24,7 +31,6 @@ for p in proudcts:
 
 #寫入檔案
 with open('proudcts.csv', 'w', encoding='utf-8') as f: 
-
     f.write('商品,價格\n') 
     for p in proudcts:
         f.write(p[0] + ',' + p[1] + '\n')
