6d70de4c0fb3d3d3926bd56eac8af146baec26d8
Sedriskor <comodexit@gmail.com>
2021-02-10 11:28:11 +0800

    ver.2 add for loop

--- proudct_list.py
+++ proudct_list.py
@@ -1,14 +1,26 @@
 
 proudcts = []
+x = 0
 while True:
     name = input('請輸入商品名稱： ')
     if name == 'q':
         break
     price = input('請輸入商品價格： ')
-    p = []
-    p.append('商品：' + name)
-    p.append('價格：' + price + '元') 
-    proudcts.append(p)#建立雙重清單(二維清單)
+    # p = [] #小清單
+    # p.append('商品：' + name) 
+    # p.append('價格：' + price + '元') 
+    # proudcts.append(p)#建立雙重清單(二維清單)
+    proudcts.append(['商品：' + name, '價格：' + price + '元']) #簡寫
+    x = x + 1
 
+# print(x)
+# print(proudcts)
+# print(proudcts[0]) 
+# #大清單的第一格內容(名稱＋價錢)
+# print(proudcts[0][0])
+# #大清單的第一格裡小清單的第一格內容(價錢)
 
-print(proudcts)
+for p in proudcts:#for loop
+    print(p) #印出大清單
+    print(p[0]) ##印出小清單
+
